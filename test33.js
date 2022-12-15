dbed14c0bbc9733bbeb51dec0cca7530db92c777
Desmond <desmondmpofu@gmail.com>
2022-12-15 13:30:13 +0200

    additions

---
+++ test33.js
@@ -0,0 +1,14 @@
+
+const shape ={
+   radius: 10,
+   diameter(){
+     return this.radius*2;
+   },
+   perimeter: ()=>{
+    return 2* Math.PI* this.radius;
+   }
+
+};
+
+console.log(shape.diameter());
+console.log(shape.perimeter()); //will not work because of arrow function
